1a793927d71c7f74387a4281f3df7863fa8b62a3
Your Name <you@example.com>
2024-09-23 15:02:27 +0900

    Add all features

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,41 +1,54 @@
-import javax.xml.parsers.DocumentBuilderFactory
+import kotlinx.coroutines.*
 import org.w3c.dom.Element
 import org.w3c.dom.NodeList
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import javax.xml.parsers.DocumentBuilderFactory
 
 
-fun main() {
+data class RssItem(
+    val title: String, val description: String, val pubDate: String,
+)
+
+fun asyncXmlParser(link: String): NodeList {
     val factory = DocumentBuilderFactory.newInstance()
-    val xml = factory.newDocumentBuilder()
-        .parse("https://techblog.woowahan.com/feed")
-    val items = xml.getElementsByTagName("item")
+    val xml = factory.newDocumentBuilder().parse(link)
 
-    val num = items.length
-    println(num)
+    return xml.getElementsByTagName("item")
+}
 
-    var list = mutableListOf<Pair<String, LocalDateTime>>()
-
-    for ( i in 0 until items.length) {
-
+suspend fun getRssItemFromLink(link: String): List<RssItem> {
+    val rssItems = mutableListOf<RssItem>()
+    val items = asyncXmlParser(link)
+    for (i in 0 until items.length) {
         val item = items.item(i) as Element
 
         val title = item.getElementsByTagName("title").item(0).textContent
-        println(title)
-
+        val description = item.getElementsByTagName("description").item(0).textContent
         val pubDate = item.getElementsByTagName("pubDate").item(0).textContent
-        println(pubDate.substring(0..25).split(",")[1].trim())
 
-        // DateTimeFormatter 패턴 정의
-        val formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss Z", java.util.Locale.ENGLISH)
-
-        // 문자열을 LocalDateTime으로 파싱
-        val dateTime = LocalDateTime.parse(pubDate, formatter)
-        list.add(Pair<String, LocalDateTime>(title, dateTime))
+        rssItems.add(RssItem(title, description, pubDate))
     }
-
-    list.sortBy { it.second }
-
-    println(list)
+    return rssItems
 }
+
+fun main() {
+    val scope = CoroutineScope(Dispatchers.Default)
+    var items = listOf<RssItem>()
+    scope.launch {
+        val woowhaItems = async { getRssItemFromLink("https://techblog.woowahan.com/feed") }
+        val kakaoItems = async { getRssItemFromLink("https://tech.kakao.com/feed") }
+        items = woowhaItems.await() + kakaoItems.await()
+        items.sortedByDescending { LocalDateTime.parse(it.pubDate, DateTimeFormatter.RFC_1123_DATE_TIME) }
+    }
+    print("검색어를 입력하세요: ")
+    val searchWord = readln().trim().toString()
+    items.filter { it.title.contains(searchWord) }.forEach {
+        println(it.pubDate)
+        println(it.title)
+        println(it.description)
+        println()
+    }
+}
+
+
