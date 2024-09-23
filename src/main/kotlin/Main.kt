2a4dd1f4c56e57827af68865de2b3bc510dbb12b
Your Name <you@example.com>
2024-09-23 15:29:12 +0900

    런블럭으로 변경

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,16 +10,16 @@ data class RssItem(
     val title: String, val description: String, val pubDate: String,
 )
 
-fun asyncXmlParser(link: String): NodeList {
+fun xmlParser(link: String): NodeList {
     val factory = DocumentBuilderFactory.newInstance()
     val xml = factory.newDocumentBuilder().parse(link)
 
     return xml.getElementsByTagName("item")
 }
 
-suspend fun getRssItemFromLink(link: String): List<RssItem> {
+suspend fun asyncGetRssItemFromLink(link: String): List<RssItem> {
     val rssItems = mutableListOf<RssItem>()
-    val items = asyncXmlParser(link)
+    val items = xmlParser(link)
     for (i in 0 until items.length) {
         val item = items.item(i) as Element
 
@@ -33,14 +33,15 @@ suspend fun getRssItemFromLink(link: String): List<RssItem> {
 }
 
 fun main() {
-    val scope = CoroutineScope(Dispatchers.Default)
+    val scope = CoroutineScope(Dispatchers.IO)
     var items = listOf<RssItem>()
-    scope.launch {
-        val woowhaItems = async { getRssItemFromLink("https://techblog.woowahan.com/feed") }
-        val kakaoItems = async { getRssItemFromLink("https://tech.kakao.com/feed") }
+    runBlocking {
+        val woowhaItems = async { asyncGetRssItemFromLink("https://techblog.woowahan.com/feed") }
+        val kakaoItems = async { asyncGetRssItemFromLink("https://tech.kakao.com/feed") }
         items = woowhaItems.await() + kakaoItems.await()
         items.sortedByDescending { LocalDateTime.parse(it.pubDate, DateTimeFormatter.RFC_1123_DATE_TIME) }
     }
+
     print("검색어를 입력하세요: ")
     val searchWord = readln().trim().toString()
     items.filter { it.title.contains(searchWord) }.forEach {
