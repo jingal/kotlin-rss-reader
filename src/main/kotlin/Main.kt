f4352f9e0867f2640a30b26a8d2e6e036b37b47b
jingal <ilovejinjoo@gmail.com>
2024-09-23 14:20:15 +0900

    feat : 날짜별로 Sort까지 추가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 import javax.xml.parsers.DocumentBuilderFactory
 import org.w3c.dom.Element
 import org.w3c.dom.NodeList
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 fun main() {
@@ -12,13 +15,27 @@ fun main() {
     val num = items.length
     println(num)
 
+    var list = mutableListOf<Pair<String, LocalDateTime>>()
+
     for ( i in 0 until items.length) {
+
         val item = items.item(i) as Element
 
         val title = item.getElementsByTagName("title").item(0).textContent
         println(title)
 
         val pubDate = item.getElementsByTagName("pubDate").item(0).textContent
-        println(pubDate)
+        println(pubDate.substring(0..25).split(",")[1].trim())
+
+        // DateTimeFormatter 패턴 정의
+        val formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss Z", java.util.Locale.ENGLISH)
+
+        // 문자열을 LocalDateTime으로 파싱
+        val dateTime = LocalDateTime.parse(pubDate, formatter)
+        list.add(Pair<String, LocalDateTime>(title, dateTime))
     }
+
+    list.sortBy { it.second }
+
+    println(list)
 }
